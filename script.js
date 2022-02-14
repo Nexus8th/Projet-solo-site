c6eee08db010042185a40d4b748215dac4fe8fb7
Julien <mmos002@gmail.com>
2022-02-14 16:28:00 +0100

    Menu burger mostly done for Mobile - No transition

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const menuBtn = document.querySelector('.burger-btn');
+const burgerMenu = document.querySelector('.burger-mb');
+let menuOpen = false;
 
- 
+menuBtn.addEventListener('click', () => {
+    if(!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+        burgerMenu.classList.add('burger-mb-show');
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+        burgerMenu.classList.remove('burger-mb-show');
+    }
+});
  
