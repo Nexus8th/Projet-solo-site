2ef95b256491cb509fb8baac9e3d1dad1991df31
Julien <mmos002@gmail.com>
2022-02-16 17:45:45 +0100

    Deadline push -- Mostly Done

--- script.js
+++ script.js
@@ -13,4 +13,29 @@ menuBtn.addEventListener('click', () => {
         burgerMenu.classList.remove('burger-mb-show');
     }
 });
- 
+
+// carousel //
+
+let index = 0;
+const dots = document.getElementsByClassName("dot");
+const images = document.getElementsByClassName("image");
+showImage(index);
+
+function showImage(j){
+    index += j;
+    for (i = 0; i < images.length; i++)
+        images[i].style.display = "none";
+    
+    for (i = 0; i < dots.length; i++){
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    if (index > images.length - 1){
+            index = 0;
+    }
+    if (index < 0){
+            index = images.length - 1;}
+        
+
+    images[index].style.display = "block";
+    dots[index].className += " active";
+}
